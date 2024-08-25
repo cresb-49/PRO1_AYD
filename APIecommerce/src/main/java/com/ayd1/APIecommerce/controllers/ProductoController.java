d7898730a810bfcfccc8180c91da4f558506dd90
jaquelinecifuentes201130968@cunoc.edu.gt <jaquelinecifuentes201130968@cunoc.edu.gt>
2024-08-25 03:35:29 -0600

    rutas

--- APIecommerce/src/main/java/com/ayd1/APIecommerce/controllers/ProductoController.java
+++ APIecommerce/src/main/java/com/ayd1/APIecommerce/controllers/ProductoController.java
@@ -6,14 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ayd1.APIecommerce.models.Producto;
 import com.ayd1.APIecommerce.services.ProductoService;
 import com.ayd1.APIecommerce.transformers.ApiBaseTransformer;
-
-
 
 @RestController
 @RequestMapping("/api")
@@ -26,10 +26,24 @@ public class ProductoController {
     public ResponseEntity<?> getProdutos(@RequestParam String param) {
         return new ApiBaseTransformer(HttpStatus.OK, "OK", null, null, null).sendResponse();
     }
-    
+
     @PostMapping("/producto/{id}")
     public ResponseEntity<?> getProducto(@PathVariable Long id) {
         return new ApiBaseTransformer(HttpStatus.OK, "OK", null, null, null).sendResponse();
     }
-    
+
+    @PostMapping("/producto/private/crearProducto")
+    public ResponseEntity<?> crearUsuario(@RequestBody Producto crear) {
+        try {
+            Producto respuesta = productoService.createProducto(crear);
+            return new ApiBaseTransformer(HttpStatus.OK, "OK", respuesta,
+                    null, null).sendResponse();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ApiBaseTransformer(HttpStatus.BAD_REQUEST,
+                    ex.getMessage(),
+                    null, null, null).sendResponse();
+        }
+    }
+
 }
