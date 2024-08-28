a09b4a177b1222acd27e15af00d7b482bce1bcc5
jaquelinecifuentes201130968@cunoc.edu.gt <jaquelinecifuentes201130968@cunoc.edu.gt>
2024-08-27 20:38:23 -0600

    fix merge

--- APIecommerce/src/main/java/com/ayd1/APIecommerce/repositories/CategoriaRepository.java
+++ APIecommerce/src/main/java/com/ayd1/APIecommerce/repositories/CategoriaRepository.java
@@ -1,24 +1,11 @@
 package com.ayd1.APIecommerce.repositories;
 
-<<<<<<< HEAD
 import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-=======
->>>>>>> bffb9f9b0d6b9ec8c0e63be9e05d08a1ee924b9c
 import com.ayd1.APIecommerce.models.Categoria;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CategoriaRepository extends CrudRepository<Categoria, Long> {
 
-<<<<<<< HEAD
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-    
-    public Optional<Categoria> findOneByNombre(String nombre);
-} 
-=======
     public Optional<Categoria> findOneByNombre(String nombre);
 }
->>>>>>> bffb9f9b0d6b9ec8c0e63be9e05d08a1ee924b9c
