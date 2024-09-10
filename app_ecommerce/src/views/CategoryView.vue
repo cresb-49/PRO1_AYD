57f87d2aa0a6694ac024f38f5c6931326cb8d03a
FernandoJRR <fercitotron@gmail.com>
2024-09-10 12:11:17 -0600

    feat: finish category view of products

--- app_ecommerce/src/views/CategoryView.vue
+++ app_ecommerce/src/views/CategoryView.vue
@@ -1,22 +1,29 @@
 <template>
     <v-container>
         <h2 class="mb-2">{{ categoria }}</h2>
+        <v-btn prepend-icon="mdi-arrow-left" to="/" class="mb-3">
+            Regresar
+        </v-btn>
         <v-row>
             <v-col v-for="product in productsCategory" :key="product.id" cols="12" sm="6" md="4" lg="3">
-                <v-card class="mx-auto" max-width="300" :to='`producto/${product.id}`'>
-                    <v-img
-                        :src="product === undefined 
-                        ? notFoundLink 
+                <v-card class="mx-auto" max-width="300">
+                    <v-img :src="product === undefined
+                        ? notFoundLink
                         : product.imagenesUrls === undefined ? notFoundLink : product.imagenesUrls[0]
-                        "
-                        height="150px"></v-img>
+                        " height="150px"></v-img>
 
                     <v-card-title>{{ product.nombre }}</v-card-title>
                     <v-card-subtitle>Q{{ product.precio }}</v-card-subtitle>
-
                     <v-card-actions>
-                        <v-btn v-if="!isInCart(product)" color="primary" @click="addToCart(product)">Agregar al Carrito</v-btn>
-                        <v-chip v-else color="green" text-color="white">En Carrito</v-chip>
+                    <v-btn 
+                        v-if="!isProductInCart(product.id)" 
+                        @click="addToCart(product)"
+                        prepend-icon="mdi-cart-outline"
+                        >
+                        Agregar
+                    </v-btn>
+                    <v-chip v-else color="green" text-color="white">En Carrito</v-chip>
+                        <v-btn :to="`/producto/${product.id}`">Ver</v-btn>
                     </v-card-actions>
                 </v-card>
             </v-col>
@@ -25,6 +32,7 @@
 </template>
 
 <script setup lang="ts">
+import { useCartStore } from '@/stores/cart';
 import { useCategoryStore, type Category } from '@/stores/categories';
 import { useProductStore } from '@/stores/products';
 import { storeToRefs } from 'pinia';
@@ -33,44 +41,21 @@ import { useRoute } from 'vue-router';
 
 const notFoundLink = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png'
 
-const products = [
-    {
-        name: 'Product 1',
-        price: 29.99,
-        image: 'https://via.placeholder.com/150',
-        id: 1,
-    },
-    {
-        name: 'Product 2',
-        price: 49.99,
-        image: 'https://via.placeholder.com/150',
-        id: 2,
-    },
-    // Add more products as needed
-];
-
-const {fetchCategory} = useCategoryStore()
-const {fetchProductsByCategory} = useProductStore()
-const {productsCategory} = storeToRefs(useProductStore())
+const { fetchCategory } = useCategoryStore()
+const { fetchProductsByCategory } = useProductStore()
+const { productsCategory } = storeToRefs(useProductStore())
+const {isProductInCart, addProduct} = useCartStore()
 
 const categoria = ref('')
 
 fetchCategory(useRoute().params.id as unknown as number).then(r => r.data.value.data as Category).then(category => {
     categoria.value = category.nombre
 })
+
 fetchProductsByCategory(useRoute().params.id as unknown as number)
 
-const cart = ref<number[]>([]); // Array to store product IDs in the cart
-
 function addToCart(product: any) {
-    if (!isInCart(product)) {
-        cart.value.push(product.id);
-        console.log('Added to cart:', product);
-    }
-}
-
-function isInCart(product: any) {
-    return cart.value.includes(product.id);
+    addProduct({id: product.id, quantity: 1})
 }
 </script>
 <style scoped>
