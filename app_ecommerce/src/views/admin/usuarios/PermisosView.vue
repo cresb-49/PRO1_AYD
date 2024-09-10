14d1d82a4f181c72d49211fdac4a98d46242099a
benjamin <carlosbpac@gmail.com>
2024-09-09 19:03:47 -0600

    Refactor TableData component and add PermisosView component
    - Refactor TableData component to add margin to v-btn elements
    - Add PermisosView component for managing user permissions
    - Update UsuariosView component to include a "Permisos" action button

---
+++ app_ecommerce/src/views/admin/usuarios/PermisosView.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <header>
+            <h1 class="mb-4">Permisos Usuarios</h1>
+        </header>
+        <section>
+            
+        </section>
+    </main>
+</template>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import {useUserStore} from '@/stores/users';
+
+
+</script>
--- app_ecommerce/src/views/admin/usuarios/UsuariosView.vue
+++ app_ecommerce/src/views/admin/usuarios/UsuariosView.vue
@@ -2,45 +2,38 @@
   <main>
     <header>
       <h1 class="mb-4">Usuarios</h1>
-      <v-btn
-        prepend-icon="mdi-arrow-left"
-        to="/admin"
-        class="mt-3"
-      >
-        Regresar
-      </v-btn>
-      <v-btn
-        prepend-icon="mdi-pencil-outline"
-        to="/admin/productos/add"
-        class="mt-3"
-      >
+      <v-btn style="margin: 3px;" prepend-icon="mdi-arrow-left" to="/admin" class="mt-3"> Regresar </v-btn>
+      <v-btn style="margin: 3px;" prepend-icon="mdi-pencil-outline" to="/admin/productos/add" class="mt-3">
         Agregar Usuario
       </v-btn>
     </header>
     <section>
-      <TableData :columns="columns" :data="users" data_key="id" :actions="actionsTable"/>
+      <TableData :columns="columns" :data="users" data_key="id" :actions="actionsTable" />
     </section>
   </main>
 </template>
 <script setup lang="ts">
-import TableData from "../../../components/partials/TableData.vue";
-import { storeToRefs } from "pinia";
-import { useUserStore } from "@/stores/users";
+import TableData from '../../../components/partials/TableData.vue'
+import { storeToRefs } from 'pinia'
+import { useUserStore } from '@/stores/users'
 
-  const {fetchAllUsers} = useUserStore();
-  const {users} = storeToRefs(useUserStore());
-  
-  const consoleLog = (i: number) => {
-  }
-  
-  const columns = [
-    {name: 'Id', propertyName: 'id'},
-    {name: 'Nombres', propertyName: 'nombres'},
-    {name: 'Apellidos', propertyName: 'apellidos'},
-    {name: 'Email', propertyName: 'email'},
-    {name: 'Rol', propertyName: 'roles.0.rol.nombre'},
-  ]
-  const actionsTable = [{name: 'Editar', path: '/admin/usuarios/edit/:id'}, {name: 'Eliminar', onClick: consoleLog}]
+const { fetchAllUsers } = useUserStore()
+const { users } = storeToRefs(useUserStore())
 
-  fetchAllUsers();
+const consoleLog = (i: number) => {}
+
+const columns = [
+  { name: 'Id', propertyName: 'id' },
+  { name: 'Nombres', propertyName: 'nombres' },
+  { name: 'Apellidos', propertyName: 'apellidos' },
+  { name: 'Email', propertyName: 'email' },
+  { name: 'Rol', propertyName: 'roles.0.rol.nombre' }
+]
+const actionsTable = [
+  { name: 'Editar', path: '/admin/usuarios/edit/:id' },
+  { name: 'Eliminar', onClick: consoleLog },
+  { name: 'Permisos', path: '/admin/usuarios/permisos/:id' }
+]
+
+fetchAllUsers()
 </script>
